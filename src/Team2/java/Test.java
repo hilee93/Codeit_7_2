976e965d8c760c0b8a20f3b1166d0bd28f4c7ea4
hilee <eldh2002@gmail.com>
2025-09-19 18:13:41 +0900

    Test용 구구단 출력 프로그램

---
+++ src/Team2/java/Test.java
@@ -0,0 +1,13 @@
+package Team2.java;
+
+public class Test {
+    public static void main(String[] args) {
+        // 아무거나 추가해봅시당
+        for ( int i = 1; i < 10; i++) {
+            for ( int j = 0; j < 10; j++) {
+                System.out.print(i + " * " + j + " = " + i * j);
+                System.out.println();
+            }
+        }
+    }
+}
